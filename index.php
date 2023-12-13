9c31ec292c8eb77311a4b71822ff38ccd9e3924d
Subhashree-199 <153258285+Subhashree-199@users.noreply.github.com>
2023-12-13 11:36:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<center>
+ <form action="action.php" method="post">
+ 	
+to=
+<input type="email" name="email"><br><br>
+subject=
+<input type="text" name="sub"><br><br>
+
+message=
+<input type="text" name="msg"><br><br>
+
+<input type="submit" name="submit">
+
+soumyamishra.mishra8@gmail.com
+
+ </form>
+</center>
+ </body>
+ </html>
